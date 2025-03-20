39fc70e65dc04d451201d85cb8b033b167c7a620
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2025-03-21 00:35:04 +0800

    add open manus

---
+++ plan.py
@@ -0,0 +1,14 @@
+from app.agent.planning import PlanningAgent
+from app.tool import Terminate, ToolCollection, PlanningTool
+
+async def main():
+    request = 'I want produce a state of the art agent system for video understanding. Can you generate a comprehensive list of 30 tools in table format (name, description, sample input, sample out input, implementation suggestion) for me to implement the tool call to the agent with ease? Thanks'
+    agent = PlanningAgent(available_tools=ToolCollection(PlanningTool(), Terminate()))
+    response = await agent.run(request)
+    print(response)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
