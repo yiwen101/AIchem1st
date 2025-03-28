066a34125885c4731dac5f04779d038079f48130
Wang Yiwen <121547057+yiwen101@users.noreply.github.com>
2025-03-28 18:04:28 +0800

    implement write result node

--- app/nodes/write_result.py
+++ app/nodes/write_result.py
@@ -1,7 +1,24 @@
 
-def write_result(state):
+from AIchem1st.app.model.state import VideoAgentState
+import os
+
+def write_result(state: VideoAgentState):
     """
     Write the result to the result file.
     """
-    return state
-
+    last_qa = state["previous_QA"]
+    if last_qa is None:
+        #panic
+        raise ValueError("No previous QA found at write_result node")
+    
+    qid = state["query"].qid
+    answer = last_qa["answer"]
+    
+    #check if the result.csv file exists
+    if not os.path.exists("result.csv"):
+        with open("result.csv", "w") as f:
+            f.write("qid,pred\n")
+    
+    #append one line to the result.csv file
+    with open("result.csv", "a") as f:
+        f.write(f"{qid},{answer}\n")
